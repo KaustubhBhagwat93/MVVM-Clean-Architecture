d8a35181a178c75985052459547b378b209652d1
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-09-21 11:08:36 +0530

    updated unit tests

--- app/src/test/java/com/example/mvvmcleanarchitecture/feature_note/data/domain/use_case/DeleteNoteTest.kt
+++ app/src/test/java/com/example/mvvmcleanarchitecture/feature_note/data/domain/use_case/DeleteNoteTest.kt
@@ -2,10 +2,7 @@ package com.example.mvvmcleanarchitecture.feature_note.data.domain.use_case
 
 import com.example.mvvmcleanarchitecture.feature_note.data.domain.model.Note
 import com.example.mvvmcleanarchitecture.feature_note.data.repository.MockNoteRepository
-import com.google.common.truth.Truth
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Test
@@ -26,7 +23,7 @@ class DeleteNoteTest {
     }
 
     @Test
-    fun `add correct note, check entry`() = runBlocking {
+    fun `delete note test`() = runBlocking {
         val note =
             Note(title = "This is title", content = "This is content", timeStamp = System.currentTimeMillis(), color = 0)
         addNote(note = note)
